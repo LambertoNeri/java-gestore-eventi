a1a6bf383e4d0845d35e80d819533554ef931b01
LambertoNeri <neri.lamberto91@gmail.com>
2023-10-30 12:47:32 +0100

    First push

--- src/org/lessons/java/Event.java
+++ src/org/lessons/java/Event.java
@@ -1,2 +1,99 @@
-package org.lessons.java;public class Event {
+package org.lessons.java;
+
+import org.w3c.dom.Text;
+
+import java.time.LocalDate;
+
+public class Event {
+    public static int DEFAULT_RESERVED_SEAT = 0;
+    private String title;
+    private LocalDate date;
+    private int totalSeat;
+    private int reservedSeat;
+
+    public Event(String title, LocalDate date, int totalSeat) throws IllegalArgumentException{
+        if (dateCheck(date) == false) {
+            throw new IllegalArgumentException("La data dell'evento non è valida");
+        } else {
+            this.date = date;
+        }
+
+        if ( positiveSeat(totalSeat) == false ) {
+            throw new IllegalArgumentException("Il numero di posti totali deve essere maggiore di 0");
+        } else {
+            this.totalSeat = totalSeat;
+        }
+
+        this.title = title;
+        this.reservedSeat = DEFAULT_RESERVED_SEAT;
+
+    }
+
+    // METODI PRIVATI DI CONTROLLO DATA E POSTI TOTALI
+    private boolean dateCheck(LocalDate date) {
+        boolean isAfter = date.isAfter(LocalDate.now().minusDays(1));
+        return isAfter;
+    }
+
+    private boolean positiveSeat( int totalSeat) {
+        if (totalSeat <= 0 ) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    //METODI PUBBLICI
+    public void makeReservation(int reservedSeat) throws IllegalArgumentException{
+        if(dateCheck(date) == false) {
+            throw new IllegalArgumentException("L'evento selezionato ha già avuto luogo");
+        } else if (reservedSeat <= 0) {
+            throw new IllegalArgumentException("il numero inserito di posti da prenotare deve essere maggiore di 0");
+        } else if (reservedSeat + this.reservedSeat > totalSeat) {
+            throw new IllegalArgumentException("Il numero massimo di posti prenotabili è " + (totalSeat - this.reservedSeat));
+        } else {
+            this.reservedSeat += reservedSeat;
+        }
+    }
+
+    public void cancelReservation (int cancelReservedSeat) throws IllegalArgumentException {
+        if(dateCheck(date) == false) {
+            throw new IllegalArgumentException("L'evento selezionato ha già avuto luogo");
+        } else if (reservedSeat - cancelReservedSeat < 0) {
+            throw new IllegalArgumentException("Il numero massimo di posti cancellabili è " + (reservedSeat));
+        } else {
+            this.reservedSeat -= cancelReservedSeat;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return date + " - " + title;
+    }
+
+    //GETTERS AND SETTERS
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        dateCheck(date);
+        this.date = date;
+    }
+
+    public int getTotalSeat() {
+        return totalSeat;
+    }
+
+    public int getReservedSeat() {
+        return reservedSeat;
+    }
 }
--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -1,2 +1,50 @@
-package org.lessons.java;public class Main {
+package org.lessons.java;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+
+
+        System.out.println("Benvenuto nel creatore di eventi Lamberto Inc.");
+        System.out.println("Siete pregati di seguire le istruzioni a schermo per creare un nuovo evento: ");
+        System.out.println("Step 1) Inserire il titolo dell'evento");
+        String title = scanner.nextLine();
+        String dateStr = "";
+        LocalDate date = LocalDate.parse("01/01/2023", formatter);
+        boolean success = false;
+        while (success == false) {
+            try {
+                System.out.println("Step 2) Inserire la data nel quale si desidera ogranizzare l'evento (dd/mm/yyyy)");
+                dateStr = scanner.next();
+
+                date = LocalDate.parse(dateStr, formatter);
+                scanner.nextLine();
+            } catch (DateTimeParseException e) {
+               if (dateStr.contains("-")) {
+                   System.out.println("Perfavore inserire la data nel formato utilizzando / e non - come divisorio" );
+               }
+            } catch (IllegalArgumentException e) {
+                System.out.println(e);
+            }
+        }
+
+        System.out.println("Step 3) Inserire il numero totale di posti prenotabili");
+        int totalSeat = scanner.nextInt();
+        scanner.nextLine();
+        Event event1 = new Event(title, date, totalSeat);
+
+
+    System.out.println(event1);
+
+
+
+    }
 }
